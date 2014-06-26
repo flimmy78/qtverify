96c17b29777d16f0d57df8402e24e8331a6bbba5
yangshen <yangshen@62fcab80-fd93-7748-8c49-e77283212eb3>
2014-06-26 01:50:55 +0000

    git-svn-id: https://192.168.1.124:8443/svn/delusoft/trunk/qtverify@160 62fcab80-fd93-7748-8c49-e77283212eb3

--- source/qtexdb/qtexdb.cpp
+++ source/qtexdb/qtexdb.cpp
@@ -1,57 +1,7 @@
 #include <QtCore/QDebug>
+#include <QtCore/QSettings>
 #include "qtexdb.h"
 
-/*int GetLdfIniInfomation( LDF_INIINFO_PTR info )
-{
-	FILE *fp = NULL;
-	char filename[255];
-	char* ldfhome = getenv( "WPFHOME" );
-#ifdef __unix
-	sprintf( filename, "%s/ini/wpfini.ini", ldfhome );
-#else
-	sprintf( filename, "%s\\ini\\wpfini.ini", ldfhome );
-#endif
-
-	fp = fopen( filename, "r" );
-	printf(">>>>>>>>>>>>>>> filename=%s fp=%d %s %d\n", filename, fp, __FILE__, __LINE__);
-	if( fp!=NULL )
-	{
-		int i=0;
-		char str[255];
-		while( !feof(fp) )
-		{
-			strcpy(str, "");
-			fgets(str,125,fp);
-			str[strlen(str)-1]='\0';
-			sscanf(str, "%s", str);
-			if(str[0]== '#') continue;
-			if(!strcmp(str, "}" )) continue;
-			if(!strcmp(str, "{" ))
-			{
-				strcpy(str, "");
-				fgets(str,125,fp);
-				sscanf(str, "%s", str);
-				if(i==0) info->nMaxPointNum = atoi(str);
-				if(i==1) info->nDataBeginFlag = atoi(str);
-				if(i==2) info->nStParaSrc = atoi(str);
-				if(i==3) info->nStDataSrc = atoi(str);
-				if(i==4) info->nPrintFlag = atoi(str);
-				if(i==5) info->nWeatherFlag = atoi(str);
-				if(i==6) info->nStlfItemFlag = atoi(str);
-				if(i==7) info->nBlfLoacatedFlag = atoi(str);
-				if(i==8) info->nMlfLoacatedFlag = atoi(str);
-				if(i==9) info->nSstlfLoacatedFlag = atoi(str);
-				i++;
-			}
-		}
-		fclose(fp);
-		if(info->nDataBeginFlag!=0) info->nDataBeginFlag=1;
-		return True;
-	}
-	else{
-		return False;
-	}
-}*/
 
 int testFunc(int a, int b)
 {
@@ -60,7 +10,6 @@ int testFunc(int a, int b)
 }
 CQtExdb::CQtExdb()
 {
-
 }
 
 CQtExdb::~CQtExdb()
@@ -68,3 +17,25 @@ CQtExdb::~CQtExdb()
 
 }
 
+void CQtExdb::WriteSettings()
+{
+	QSettings settings("Option.ini", QSettings::IniFormat); // 当前目录的INI文件
+	settings.beginGroup("DevOption");
+	settings.setValue("mainFun", 25);
+	settings.setValue("subFun", 40);
+	settings.setValue("service", 1);
+	settings.endGroup();
+}
+
+void CQtExdb::ReadSettings()
+{
+	QSettings settings("Option.ini", QSettings::IniFormat);
+
+	int mainFun = settings.value("DevOption/mainFun").toInt();
+	int subFun = settings.value("DevOption/subFun").toInt();
+	QString service = settings.value("DevOption/service").toString();
+
+	qDebug() << "service" << service;
+	qDebug() << "mainFun" << mainFun;
+	qDebug() << "subFun" << subFun;
+}
