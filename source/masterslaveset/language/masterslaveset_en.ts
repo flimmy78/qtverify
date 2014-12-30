ba9f0cb0794db4846c1315f62a43f1444b8058dd
yangshen <yangshen@62fcab80-fd93-7748-8c49-e77283212eb3>
2014-12-30 01:01:55 +0000

    masterslaveset改为英文原始界面，增加中英文词典
    git-svn-id: https://192.168.1.124:8443/svn/delusoft/trunk/qtverify@373 62fcab80-fd93-7748-8c49-e77283212eb3

---
+++ source/masterslaveset/language/masterslaveset_en.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>CMasterSlave</name>
+    <message>
+        <location filename="../source/masterslaveset.cpp" line="86"/>
+        <source>Hint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../source/masterslaveset.cpp" line="86"/>
+        <source>Save Settings Successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MasterSlaveClass</name>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="14"/>
+        <source>HostSlaveSet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="26"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="39"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="52"/>
+        <source>NetMode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="71"/>
+        <source>host</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="91"/>
+        <source>slave</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="107"/>
+        <source>Slave:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="113"/>
+        <source>Slave1 IP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="136"/>
+        <location filename="../ui/masterslaveset.ui" line="169"/>
+        <location filename="../ui/masterslaveset.ui" line="202"/>
+        <location filename="../ui/masterslaveset.ui" line="235"/>
+        <source>hostname:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="146"/>
+        <source>Slave2 IP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="179"/>
+        <source>Slave3 IP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="212"/>
+        <source>Slave4 IP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/masterslaveset.ui" line="254"/>
+        <source>Host：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/masterslaveset.ui" line="260"/>
+        <source>Host IP:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/masterslaveset.ui" line="283"/>
+        <source>hostname：</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
