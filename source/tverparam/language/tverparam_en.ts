6f4790f81c2ba01a469b67211a2a075c6593bcfa
songbaoshan <songbaoshan@62fcab80-fd93-7748-8c49-e77283212eb3>
2015-05-07 02:25:34 +0000

    铂电阻参数法 语言包
    git-svn-id: https://192.168.1.124:8443/svn/delusoft/trunk/qtverify@610 62fcab80-fd93-7748-8c49-e77283212eb3

---
+++ source/tverparam/language/tverparam_en.ts
@@ -0,0 +1,188 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>PlaParamDlgClass</name>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="14"/>
+        <source>铂电阻参数法检测</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="41"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:11pt; font-weight:600;&quot;&gt;Calculation&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="54"/>
+        <source>Result for single sensor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="165"/>
+        <location filename="../ui/tverparam.ui" line="208"/>
+        <location filename="../ui/tverparam.ui" line="252"/>
+        <location filename="../ui/tverparam.ui" line="495"/>
+        <location filename="../ui/tverparam.ui" line="865"/>
+        <location filename="../ui/tverparam.ui" line="1080"/>
+        <location filename="../ui/tverparam.ui" line="1282"/>
+        <source>In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="175"/>
+        <location filename="../ui/tverparam.ui" line="197"/>
+        <location filename="../ui/tverparam.ui" line="219"/>
+        <location filename="../ui/tverparam.ui" line="241"/>
+        <location filename="../ui/tverparam.ui" line="263"/>
+        <location filename="../ui/tverparam.ui" line="285"/>
+        <source>Err</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="186"/>
+        <location filename="../ui/tverparam.ui" line="230"/>
+        <location filename="../ui/tverparam.ui" line="274"/>
+        <location filename="../ui/tverparam.ui" line="500"/>
+        <location filename="../ui/tverparam.ui" line="870"/>
+        <location filename="../ui/tverparam.ui" line="1085"/>
+        <location filename="../ui/tverparam.ui" line="1287"/>
+        <source>Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="320"/>
+        <location filename="../ui/tverparam.ui" line="701"/>
+        <source>1st temperature head</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="343"/>
+        <location filename="../ui/tverparam.ui" line="382"/>
+        <location filename="../ui/tverparam.ui" line="434"/>
+        <source>Err Lim(%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="395"/>
+        <source>2nd temperature head</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="447"/>
+        <location filename="../ui/tverparam.ui" line="1105"/>
+        <source>3rd temperature head</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="462"/>
+        <source>Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="505"/>
+        <source>δT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="610"/>
+        <source>In
+(℃)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="616"/>
+        <source>Out
+(℃)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="622"/>
+        <source>δT
+(%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="657"/>
+        <source>Err Lim℃/%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="688"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:11pt; font-weight:600;&quot;&gt;Data Input&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="713"/>
+        <location filename="../ui/tverparam.ui" line="915"/>
+        <location filename="../ui/tverparam.ui" line="1117"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Stand temperature&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="742"/>
+        <location filename="../ui/tverparam.ui" line="970"/>
+        <location filename="../ui/tverparam.ui" line="1172"/>
+        <source>R(Ω)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="747"/>
+        <location filename="../ui/tverparam.ui" line="975"/>
+        <location filename="../ui/tverparam.ui" line="1177"/>
+        <source>T(℃)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="771"/>
+        <location filename="../ui/tverparam.ui" line="928"/>
+        <location filename="../ui/tverparam.ui" line="1130"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Checked Sensor&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="860"/>
+        <location filename="../ui/tverparam.ui" line="1075"/>
+        <location filename="../ui/tverparam.ui" line="1277"/>
+        <source>Serial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="889"/>
+        <location filename="../ui/tverparam.ui" line="941"/>
+        <location filename="../ui/tverparam.ui" line="1143"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;(Input R following Ω)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="903"/>
+        <source>2ndt temperature head</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="1308"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="1325"/>
+        <source>Config</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="1342"/>
+        <source>Excel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/tverparam.ui" line="1359"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; color:#0000ff;&quot;&gt;Step：1.Configurate 2.Input Data 3.Out to excel 4.Save Data 5.Exit&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/tverparam.ui" line="1372"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
