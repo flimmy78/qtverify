89e7c2e19bc35bcbc6241f039e419aa142a0ba82
yangshen <yangshen@62fcab80-fd93-7748-8c49-e77283212eb3>
2015-10-19 08:42:30 +0000

    增加功能：编辑制造单位、送检单位、表型号、装置信息等
    git-svn-id: https://192.168.1.124:8443/svn/delusoft/trunk/qtverify@1204 62fcab80-fd93-7748-8c49-e77283212eb3

--- source/include/usermanagedlg.h
+++ source/include/usermanagedlg.h
@@ -31,17 +31,50 @@ public:
 	~UserManageDlg();
 
 	QSqlRelationalTableModel *m_userModel;
+	QSqlTableModel *m_manudeptModel;
+	QSqlTableModel *m_verifydeptModel;
+	QSqlTableModel *m_modelModel;
+	QSqlTableModel *m_deviceModel;
 
 	void initUserList();
+	void initManuDept();
+	void initVerifyDept();
+	void initMeterModel();
+	void initDeviceInfo();
 
 private slots:
+	void on_btnExit_clicked();
+
+	//T_User_Def_Tab
 	void on_btnSubmit_clicked();
 	void on_btnRevert_clicked();
 	void on_btnAdd_clicked();
 	void on_btnDelete_clicked();
-	void on_btnExit_clicked();
-
 	void on_userTableView_clicked(const QModelIndex &);
+
+	//T_Manufacture_Dept
+	void on_btnSubmit_2_clicked();
+	void on_btnRevert_2_clicked();
+	void on_btnAdd_2_clicked();
+	void on_btnDelete_2_clicked();
+
+	//T_Verify_Dept
+	void on_btnSubmit_3_clicked();
+	void on_btnRevert_3_clicked();
+	void on_btnAdd_3_clicked();
+	void on_btnDelete_3_clicked();
+
+	//T_Meter_Model
+	void on_btnSubmit_4_clicked();
+	void on_btnRevert_4_clicked();
+	void on_btnAdd_4_clicked();
+	void on_btnDelete_4_clicked();
+
+	//T_Verify_Device_Info
+	void on_btnSubmit_5_clicked();
+	void on_btnRevert_5_clicked();
+	void on_btnAdd_5_clicked();
+	void on_btnDelete_5_clicked();
 
 private:
 	Ui::UserManageDlgClass ui;	
